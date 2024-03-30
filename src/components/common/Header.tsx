c7e11113feeb409db4b10474fdbffa3d591cf428
Sumant Shah <sumant.shah@gigacars.com>
2024-03-30 19:21:50 +0530

    faqs section

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,32 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import React from 'react'
+import { tokens } from '../../theme';
+
+interface Props {
+    title: string;
+    subtitle: string;
+}
+
+const Header = (props: Props) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+
+  return (
+    <Box mb="30px">
+        <Typography
+            variant='h2'
+            color={colors.grey[100]}
+            fontWeight="bold"
+            sx={{m: '0 0 5px 0'}}
+        >
+            {props.title}
+        </Typography>
+        <Typography variant='h5' color={colors.greenAccent[400]}>
+            {props.subtitle}
+        </Typography>
+    </Box>
+  )
+}
+
+export default Header
