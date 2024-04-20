d535a17f1bf55b441f72becdba42927154f11c8f
Sumant Shah <sumant.shah@gigacars.com>
2024-04-20 18:28:25 +0530

    pie chart

--- src/scenes/pie/index.tsx
+++ src/scenes/pie/index.tsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
+import PieChart from '../../components/PieChart'
 
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <Box m="20px">
+      <Header title='Pie Chart' subtitle='Simple Pie Chart' />
+      <Box sx={{height: {xs: '70vh', md: '75vh'}}}>
+        <PieChart />
+      </Box>
+    </Box>
   )
 }
 
