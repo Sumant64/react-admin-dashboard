6c156bf2dcdbf53cdc3988c0a0a59ddd046c62b7
Sumant Shah <sumant.shah@gigacars.com>
2024-04-20 18:44:18 +0530

    bar chart

--- src/scenes/bar/index.tsx
+++ src/scenes/bar/index.tsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
+import BarChart from '../../components/BarChart'
 
 const Bar = () => {
   return (
-    <div>Bar</div>
+    <Box m="20px">
+      <Header title='Bar Chart' subtitle='Simple Bar Chart' />
+      <Box height="75vh" sx={{height: {xs: '60vh', md: '75vh'}, width: {xs: '500px', md: '100%'}}}>
+        <BarChart />
+      </Box>
+    </Box>
   )
 }
 
