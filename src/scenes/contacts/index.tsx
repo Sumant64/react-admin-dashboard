d8b837ff1920b09b4552b94ec07f55861d256fae
Sumant Shah <sumant.shah@gigacars.com>
2024-04-20 17:45:48 +0530

    contact page setup

--- src/scenes/contacts/index.tsx
+++ src/scenes/contacts/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { mockDataContacts } from '../../data/mockData';
+import { useTheme } from '@mui/material';
+import { tokens } from '../../theme';
 
 const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  
+
   return (
     <div>Contacts</div>
   )
